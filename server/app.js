d27e610c5864c742008c974f52b8b4344f4e69a6
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-29 20:57:09 +0100

    Server returns 'ok'

--- server/app.js
+++ server/app.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const csvWriter = createCsvWriter({
+    path: 'log.csv',
+    header: [
+        { id: 'agent', title: 'Agent' },
+    { id: 'time', title: 'Time' },
+    { id: 'method', title: 'Method' },
+    { id: 'resource', title: 'Resource' },
+    { id: 'version', title: 'Version' },
+    { id: 'status', title: 'Status' },
+  ]
+});
 
 app.use((req, res, next) => {
 // write your logging code here
-
+const logEntry = {
+    agent: req.headers['user-agent'],
+    time: new Date().toISOString(),
+    method: req.method,
+    resource: req.originalUrl,
+    version: req.httpVersion,
+    status: res.statusCode,
+  };
+  csvWriter.writeRecords([logEntry]).then(() => {
+    next();
+  }).catch((error) => {
+    console.error('Error writing log entry:', error);
+    next();
+  });
 });
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
-('ok')
+res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
